4b39ef9f4e1d7b3c9b3af244c82b2b4bd4cf1813
Your Name <you@example.com>
2025-10-05 15:53:19 -0300

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,24 +43,139 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int is_valid(Node* n){
+int is_valid(Node* n) 
+{
+    int i;
+    int j;
+    int num;
+    for (i = 0; i < 9; i++) 
+    {
+        int revisarfila[10] = {0};  
+        for (j = 0; j < 9; j++) {
+            num = n->sudo[i][j];
+            if (num != 0) {  
+                if (revisarfila[num] == 1) 
+                {  
+                    return 0;
+                }
+                revisarfila[num] = 1;  
+            }
+        }
+    }
+    
+    for (j = 0; j < 9; j++) {
+        int revisarcol[10] = {0};  
+        for (i = 0; i < 9; i++) {
+            num = n->sudo[i][j];
+            if (num != 0) { 
+                if (revisarcol[num] == 1) 
+                {  
+                    return 0;
+                }
+                revisarcol[num] = 1; 
+            }
+        }
+    }
+    
+    for (int k = 0; k < 9; k++) 
+    {
+        int revisarsubmatriz[10] = {0};  
+        for (int p = 0; p < 9; p++) 
+        {
+            int i = 3 * (k / 3) + (p / 3);  
+            int j = 3 * (k % 3) + (p % 3);  
+            
+            num = n->sudo[i][j];
+            if (num != 0) 
+            {  
+                if (revisarsubmatriz[num] == 1) 
+                {  
+                    return 0;
+                }
+                revisarsubmatriz[num] = 1; 
+            }
+        }
+    }
 
-    return 1;
+    return 1;  
 }
 
 
-List* get_adj_nodes(Node* n){
-    List* list=createList();
+List* get_adj_nodes(Node* n)
+{
+  List* list=createList();
+
+  int fila = -1;
+  int col = -1;
+  for (int i = 0; i < 9 && fila == -1; i++) 
+  {
+    for (int j = 0; j < 9 && col == -1; j++) 
+    {
+      if (n->sudo[i][j] == 0) {  
+        fila = i;
+        col = j;
+        break;  
+      }
+    }
+  }
+
+  if (fila == -1 || col == -1) 
+  {
+    return list;
+  }
+
+  for (int k = 1; k <= 9; k++) 
+  {
+    Node* copia = copy(n);  
+    copia->sudo[fila][col] = k;  
+    
+
+    if (is_valid(copia)) {  
+      pushBack(list, copia);  
+    } else {
+      free(copia);  
+    }
+  }
+    
     return list;
 }
 
 
 int is_final(Node* n){
-    return 0;
+    if (n == NULL) return 0;
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++)
+        {
+            if (n->sudo[i][j] == 0) return 0;
+        }
+    }
+    return 1;
 }
 
-Node* DFS(Node* initial, int* cont){
-  return NULL;
+Node* DFS(Node* n, int* cont)
+{
+    if (cont) *cont = 0;
+    if (n == NULL) return NULL;
+
+    Stack* pila = createStack();
+    push(pila, n);
+
+    while (!is_empty(pila))
+    {
+        Node* actual = (Node*) top(pila);
+        pop(pila);
+        if (cont) (*cont)++;
+
+        if (is_final(actual)) return actual;
+
+        List* adj = get_adj_nodes(actual);
+        for (Node* aux = firstList(adj); aux != NULL; aux = nextList(adj)){
+            push(pila, aux);
+        }
+
+        free(actual);
+    }
+    return NULL;  
 }
 
 
